caa00a332807389287458ea78697457dd60f59f8
gursimrangillnotinsane <132660017+gursimrangillnotinsane@users.noreply.github.com>
2025-01-21 16:37:16 -0500

    initial

---
+++ components/Header/menuData.tsx
@@ -0,0 +1,84 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "About",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "Book Now",
+    path: " https://wa.me/4379862492?text=I'm%20here%20to%20book%20a%20ride.%0A%0APickup%20Address:%20[Your%20Pickup%20Address]%0ADropoff%20Address:%20[Your%20Dropoff%20Address]%0ADeadline%20to%20Reach:%20[Your%20Deadline%20Time]",
+    newTab: true,
+  },
+  {
+    id: 3,
+    title: "Support",
+    path: "/contact",
+    newTab: false,
+  },
+  // {
+  //   id: 4,
+  //   title: "Pages",
+  //   newTab: false,
+  //   submenu: [
+  //     {
+  //       id: 41,
+  //       title: "About Page",
+  //       path: "/about",
+  //       newTab: false,
+  //     },
+  //     {
+  //       id: 42,
+  //       title: "Contact Page",
+  //       path: "/contact",
+  //       newTab: false,
+  //     },
+  //     {
+  //       id: 43,
+  //       title: "Blog Grid Page",
+  //       path: "/blog",
+  //       newTab: false,
+  //     },
+  //     {
+  //       id: 44,
+  //       title: "Blog Sidebar Page",
+  //       path: "/blog-sidebar",
+  //       newTab: false,
+  //     },
+  //     {
+  //       id: 45,
+  //       title: "Blog Details Page",
+  //       path: "/blog-details",
+  //       newTab: false,
+  //     },
+  //     {
+  //       id: 46,
+  //       title: "Sign In Page",
+  //       path: "/signin",
+  //       newTab: false,
+  //     },
+  //     {
+  //       id: 47,
+  //       title: "Sign Up Page",
+  //       path: "/signup",
+  //       newTab: false,
+  //     },
+  //     {
+  //       id: 48,
+  //       title: "Error Page",
+  //       path: "/error",
+  //       newTab: false,
+  //     },
+  //   ],
+  // },
+];
+export default menuData;
---
+++ components/Pricing/booking.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+
+const booking = () => {
+    return (
+        <section className='w-full bg-primary text-white'>
+            <div className='flex justify-center items-center p-8'>
+                <h2 className='text-xl'>Book Before 6 PM for Next Day</h2>
+                <button onClick={() => window.open('https://wa.me/4379862492?text=I\'m%20here%20to%20book%20a%20ride.%0A%0APickup%20Address:%20[Your%20Pickup%20Address]%0ADropoff%20Address:%20[Your%20Dropoff%20Address]%0ADeadline%20to%20Reach:%20[Your%20Deadline%20Time]')} className='bg-white text-primary p-2 rounded-md ml-4'>Book Now</button>
+            </div>
+        </section>
+    )
+}
+
+export default booking
